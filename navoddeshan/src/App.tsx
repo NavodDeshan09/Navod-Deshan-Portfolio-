33c58b63ef603a34ff3b344593a5cb5ec073811d
Wickramaarachchi W.A.N.D <navoddeshan@gmail.com>
2025-09-21 10:16:22 +0530

    update the theme

--- navoddeshan/src/App.tsx
+++ navoddeshan/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Topnavigation from "./components/Topnavigation";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
         {/* Add more routes here as needed */}
       </Routes>
     </Router>
